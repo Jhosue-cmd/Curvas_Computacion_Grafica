bdb8f599bbc627661c1e8321b02e0fded41f4913
Jhosue-cmd <jdguayasamin2@espe.edu.ec>
2025-12-13 12:30:00 -0500

    Cruva_Bezier

--- Curvas/view/frmBezier.Designer.cs
+++ Curvas/view/frmBezier.Designer.cs
@@ -1,16 +1,11 @@
-﻿namespace CURVAS.Curvas.view
+﻿
+namespace CURVAS.Curvas.view
+
 {
     partial class frmBezier
     {
-        /// <summary>
-        /// Required designer variable.
-        /// </summary>
         private System.ComponentModel.IContainer components = null;
 
-        /// <summary>
-        /// Clean up any resources being used.
-        /// </summary>
-        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
         protected override void Dispose(bool disposing)
         {
             if (disposing && (components != null))
@@ -22,18 +17,102 @@
 
         #region Windows Form Designer generated code
 
-        /// <summary>
-        /// Required method for Designer support - do not modify
-        /// the contents of this method with the code editor.
-        /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.picLienzo = new System.Windows.Forms.PictureBox();
+            this.pnlControles = new System.Windows.Forms.Panel();
+            this.btnLimpiar = new System.Windows.Forms.Button();
+            this.btnAnimar = new System.Windows.Forms.Button();
+            this.lblInfo = new System.Windows.Forms.Label();
+
+            ((System.ComponentModel.ISupportInitialize)(this.picLienzo)).BeginInit();
+            this.pnlControles.SuspendLayout();
+            this.SuspendLayout();
+
+            // 
+            // picLienzo
+            // 
+            this.picLienzo.BackColor = System.Drawing.Color.White;
+            this.picLienzo.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.picLienzo.Location = new System.Drawing.Point(0, 30);
+            this.picLienzo.Name = "picLienzo";
+            this.picLienzo.Size = new System.Drawing.Size(800, 510);
+            this.picLienzo.TabIndex = 2;
+            this.picLienzo.TabStop = false;
+            this.picLienzo.MouseDown += new System.Windows.Forms.MouseEventHandler(this.PicLienzo_MouseDown);
+            this.picLienzo.MouseMove += new System.Windows.Forms.MouseEventHandler(this.PicLienzo_MouseMove);
+            this.picLienzo.MouseUp += new System.Windows.Forms.MouseEventHandler(this.PicLienzo_MouseUp);
+            this.picLienzo.Paint += new System.Windows.Forms.PaintEventHandler(this.PicLienzo_Paint);
+
+            // 
+            // pnlControles
+            // 
+            this.pnlControles.BackColor = System.Drawing.Color.LightGray;
+            this.pnlControles.Controls.Add(this.btnLimpiar);
+            this.pnlControles.Controls.Add(this.btnAnimar);
+            this.pnlControles.Dock = System.Windows.Forms.DockStyle.Bottom;
+            this.pnlControles.Location = new System.Drawing.Point(0, 540);
+            this.pnlControles.Name = "pnlControles";
+            this.pnlControles.Size = new System.Drawing.Size(800, 60);
+            this.pnlControles.TabIndex = 1;
+
+            // 
+            // btnLimpiar
+            // 
+            this.btnLimpiar.Location = new System.Drawing.Point(190, 15);
+            this.btnLimpiar.Name = "btnLimpiar";
+            this.btnLimpiar.Size = new System.Drawing.Size(120, 30);
+            this.btnLimpiar.TabIndex = 1;
+            this.btnLimpiar.Text = "Limpiar Todo";
+            this.btnLimpiar.UseVisualStyleBackColor = true;
+            this.btnLimpiar.Click += new System.EventHandler(this.btnLimpiar_Click);
+
+            // 
+            // btnAnimar
+            // 
+            this.btnAnimar.Location = new System.Drawing.Point(20, 15);
+            this.btnAnimar.Name = "btnAnimar";
+            this.btnAnimar.Size = new System.Drawing.Size(150, 30);
+            this.btnAnimar.TabIndex = 0;
+            this.btnAnimar.Text = "Iniciar Animación";
+            this.btnAnimar.UseVisualStyleBackColor = true;
+            this.btnAnimar.Click += new System.EventHandler(this.btnAnimar_Click);
+
+            // 
+            // lblInfo
+            // 
+            this.lblInfo.Dock = System.Windows.Forms.DockStyle.Top;
+            this.lblInfo.Font = new System.Drawing.Font("Arial", 10F, System.Drawing.FontStyle.Bold);
+            this.lblInfo.Location = new System.Drawing.Point(0, 0);
+            this.lblInfo.Name = "lblInfo";
+            this.lblInfo.Size = new System.Drawing.Size(800, 30);
+            this.lblInfo.TabIndex = 0;
+            this.lblInfo.Text = "Click Izq: Agregar Punto | Arrastrar: Mover | Click Der: Borrar Último";
+            this.lblInfo.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+
+            // 
+            // frmBezier
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Text = "frmBezier";
+            this.ClientSize = new System.Drawing.Size(800, 600);
+            this.Controls.Add(this.picLienzo);
+            this.Controls.Add(this.pnlControles);
+            this.Controls.Add(this.lblInfo);
+            this.Name = "frmBezier";
+            this.Text = "Simulación Curvas de Bézier";
+            ((System.ComponentModel.ISupportInitialize)(this.picLienzo)).EndInit();
+            this.pnlControles.ResumeLayout(false);
+            this.ResumeLayout(false);
         }
 
         #endregion
+
+        private System.Windows.Forms.PictureBox picLienzo;
+        private System.Windows.Forms.Panel pnlControles;
+        private System.Windows.Forms.Button btnLimpiar;
+        private System.Windows.Forms.Button btnAnimar;
+        private System.Windows.Forms.Label lblInfo;
     }
+
 }
--- Curvas/view/frmBezier.cs
+++ Curvas/view/frmBezier.cs
@@ -1,20 +1,137 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.ComponentModel;
-using System.Data;
 using System.Drawing;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+using System.Drawing.Drawing2D;
 using System.Windows.Forms;
+using CURVAS.Curvas.model;
 
 namespace CURVAS.Curvas.view
 {
+
     public partial class frmBezier : Form
     {
+        private List<PointF> puntosControl;
+        private CBezier logicaBezier;
+        private int indiceArrastre = -1;
+        private bool animando = false;
+        private float tAnimacion = 0f;
+        private Timer tmrAnimacion;
+
         public frmBezier()
         {
-            InitializeComponent();
+            InitializeComponent(); // Llama al método en el Designer.cs
+
+            puntosControl = new List<PointF>();
+            logicaBezier = new CBezier();
+
+            // Configurar Timer
+            tmrAnimacion = new Timer();
+            tmrAnimacion.Interval = 30;
+            tmrAnimacion.Tick += TmrAnimacion_Tick;
+        }
+
+        private void btnAnimar_Click(object sender, EventArgs e)
+        {
+            if (puntosControl.Count < 2) { MessageBox.Show("Agregue al menos 2 puntos."); return; }
+            animando = !animando;
+            tAnimacion = 0;
+            btnAnimar.Text = animando ? "Detener Animación" : "Iniciar Animación";
+            tmrAnimacion.Enabled = animando;
+            picLienzo.Invalidate();
+        }
+
+        private void btnLimpiar_Click(object sender, EventArgs e)
+        {
+            puntosControl.Clear();
+            animando = false;
+            tmrAnimacion.Stop();
+            btnAnimar.Text = "Iniciar Animación";
+            picLienzo.Invalidate();
+        }
+
+        private void TmrAnimacion_Tick(object sender, EventArgs e)
+        {
+            tAnimacion += 0.01f;
+            if (tAnimacion > 1f) tAnimacion = 0f;
+            picLienzo.Invalidate();
+        }
+
+        private void PicLienzo_MouseDown(object sender, MouseEventArgs e)
+        {
+            if (e.Button == MouseButtons.Left)
+            {
+                for (int i = 0; i < puntosControl.Count; i++)
+                {
+                    if (Distancia(puntosControl[i], e.Location) < 10)
+                    {
+                        indiceArrastre = i;
+                        return;
+                    }
+                }
+                if (e.X >= 0 && e.Y >= 0)
+                    puntosControl.Add(e.Location);
+            }
+            else if (e.Button == MouseButtons.Right && puntosControl.Count > 0)
+            {
+                puntosControl.RemoveAt(puntosControl.Count - 1);
+            }
+            picLienzo.Invalidate();
+        }
+
+        private void PicLienzo_MouseMove(object sender, MouseEventArgs e)
+        {
+            if (indiceArrastre != -1)
+            {
+                float x = Math.Max(0, Math.Min(picLienzo.Width, e.X));
+                float y = Math.Max(0, Math.Min(picLienzo.Height, e.Y));
+                puntosControl[indiceArrastre] = new PointF(x, y);
+                picLienzo.Invalidate();
+            }
+        }
+
+        private void PicLienzo_MouseUp(object sender, MouseEventArgs e)
+        {
+            indiceArrastre = -1;
+        }
+
+        private void PicLienzo_Paint(object sender, PaintEventArgs e)
+        {
+            e.Graphics.SmoothingMode = SmoothingMode.AntiAlias;
+
+            if (puntosControl.Count > 0)
+            {
+                if (puntosControl.Count > 1)
+                    e.Graphics.DrawLines(Pens.LightGray, puntosControl.ToArray());
+
+                foreach (var p in puntosControl)
+                {
+                    e.Graphics.FillEllipse(Brushes.Red, p.X - 4, p.Y - 4, 8, 8);
+                    e.Graphics.DrawEllipse(Pens.Black, p.X - 4, p.Y - 4, 8, 8);
+                }
+            }
+
+            if (puntosControl.Count >= 2)
+            {
+                List<PointF> curva = new List<PointF>();
+                for (float t = 0; t <= 1; t += 0.005f)
+                    curva.Add(logicaBezier.CalcularPunto(t, puntosControl));
+
+                curva.Add(logicaBezier.CalcularPunto(1f, puntosControl));
+
+                e.Graphics.DrawLines(new Pen(Color.Blue, 2), curva.ToArray());
+
+                if (animando)
+                {
+                    PointF ptAnim = logicaBezier.CalcularPunto(tAnimacion, puntosControl);
+                    e.Graphics.FillEllipse(Brushes.LimeGreen, ptAnim.X - 6, ptAnim.Y - 6, 12, 12);
+                    e.Graphics.DrawString($"t={tAnimacion:0.00}", this.Font, Brushes.Black, ptAnim.X + 10, ptAnim.Y);
+                }
+            }
+        }
+
+        private float Distancia(PointF p1, PointF p2)
+        {
+            return (float)Math.Sqrt(Math.Pow(p1.X - p2.X, 2) + Math.Pow(p1.Y - p2.Y, 2));
         }
     }
 }
